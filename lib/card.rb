0a6a4ef2da2d73ca0e7e543cde6829c80af56c0b
Alice Boone <alicegbonel@gmail.com>
2020-09-28 22:18:39 -0700

    Wave 1

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,42 @@
-
 # card.rb
+
+VALID_SUITES = [:hearts, :spades, :clubs, :diamonds]
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+
+    if value < 1 || value > 13
+      raise ArgumentError, "Invalid Card"
+    end
+
     @value = value
     @suit = suit
-    
 
+    valid_cards = [:hearts, :spades, :clubs, :diamonds]
+    unless valid_cards.include? suit
+      raise ArgumentError, "Invalid suit"
+    end
+    end
+
+  def face_cards
+    if @value == 1
+     return "Ace"
+    elsif @value == 11
+     return "Jack"
+    elsif @value == 12
+     return "Queen"
+    elsif @value == 13
+     return "King"
+    end
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    if [1,11,12,13].include?(value)
+      return "#{self.face_cards} of #{suit.to_s}"
+    else
+      return "#{value} of #{suit.to_s}"
+    end
   end
-
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -18,41 +18,51 @@ describe Card do
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
+      expect {Card.new(1, :clovers)}.must_raise ArgumentError
     end
   end
 
-  describe "Testing to_s" do
-
-    it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
-    end
-
-    it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
-      # The current implementation of to_s does not address this feature
-      # Write the test, see it fail, then modify to_s to make it pass!
-      # (Consider writing a helper method!)
-      #
-      # For reference:
-      #  1: Ace
-      #  11: Jack
-      #  12: Queen
-      #  13: King
-    end
+  it "to_s returns a readable String value logically for values 2-10" do
+    # Test to ensure that to_s works for cards values 2-10
+    # for example:  "2 of diamonds"
+    expect(Card.new(10, :diamonds).to_s).must_equal "10 of diamonds"
+    expect(Card.new(5, :hearts).to_s).must_equal "5 of hearts"
+    expect(Card.new(2, :spades).to_s).must_equal "2 of spades"
   end
 
-  describe "Reader methods" do
+  it "to_s returns a readable String value for Ace, Jack, Queen, King" do
+    # Test to ensure that to_s works for cards values 1, and 11-13
+    # For example: "Queen of hearts"
+    # The current implementation of to_s does not address this feature
+    # Write the test, see it fail, then modify to_s to make it pass!
+    # (Consider writing a helper method!)
+    #
+    # For reference:
+    #  1: Ace
+    #  11: Jack
+    #  12: Queen
+    #  13: King
 
-    it "Can retrieve the value of the card using a `.value`." do
-      # ensure that `.value works as expected`
-    end
+    expect(Card.new(13, :hearts).to_s).must_equal "King of hearts"
+    expect(Card.new(1, :spades).to_s).must_equal "Ace of spades"
+    expect(Card.new(12, :diamonds).to_s).must_equal "Queen of diamonds"
+  end
+end
 
-    it "Can retrieve the value of the card using a `.suit`." do
-      # ensure that `.suit works as expected returning the symbol of the suit`
+describe "Reader methods" do
 
-    end
+  it "Can retrieve the value of the card using a `.value`." do
+    # ensure that `.value works as expected`
+    card = Card.new(5, :diamonds)
+
+    expect(card.value).must_equal 5
   end
 
+  it "Can retrieve the value of the card using a `.suit`." do
+    # ensure that `.suit works as expected returning the symbol of the suit`
+    card = Card.new(10, :clubs)
+
+    expect(card.suit).must_equal :clubs
+
+  end
 end
